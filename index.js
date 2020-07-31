21ba53c215be606ab4a07bce65b5093c23170406
JRIGS <raczkowskijonathan@gmail.com>
2020-07-31 11:08:24 -0400

    Quick Update

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+// function myFunction(){
+//   document.getElementById("hamburger").addEventListener("click",(ham)=>{
+//     console.log("Working"); 
+//     if (ham.style.display === "block") {
+//     ham.style.display = "none";
+//   } else {
+//     ham.style.display = "block";
+//   }})};
+
+
 function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
+}
